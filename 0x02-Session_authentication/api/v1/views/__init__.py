d4c2df3da5109948d6f007757811d00a44a4af0f
kelvinehiz <kelvinehiz@gmail.com>
2024-08-09 04:47:43 +0000

    refactored session_auth file

--- 0x02-Session_authentication/api/v1/views/__init__.py
+++ 0x02-Session_authentication/api/v1/views/__init__.py
@@ -1,2 +1,8 @@
-# api/v1/views/__init__.py
-from .app_views import app_views
+#!/usr/bin/env python3
+"""Initialize the views module."""
+from api.v1.views.app_views import app_views
+from api.v1.views.session_auth import session_auth
+
+# Register the Blueprints
+# Register session_auth with a URL prefix to ensure it's correctly scoped
+app_views.register_blueprint(session_auth, url_prefix='/api/v1')
